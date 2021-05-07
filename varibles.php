e2220e7f6769f45cf7a0be9a2110f68b0c53ba0e
bro abbas <75854041+abbasalkabbi@users.noreply.github.com>
2021-05-08 00:49:32 +0300

    Create varibles.php

---
+++ varibles.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<?php
+$name1="abbas alkaabi";
+$age1=18;
+
+$name2="hussin ";
+$age2=22;
+echo  "name:" . $name1;
+echo "<br/>";
+echo "age:" .$age1;
+
+echo "<br/>";
+
+echo   "name:" .  $name2;
+echo "<br/>";
+echo "age:" .$age2;
+
+$fullName=$name1;
+echo "<br/>";
+echo  "fullName:" . $fullName;
+
+$pi=3.4;
+echo "<br/>";
+echo "pi:".$pi;
+?> 
+
+</body>
+</html>
